3cc4a7bb4c61da209f00cd1bd08f9a39dce0cb07
Ameybairolu <ameybairolu@gmail.com>
2021-11-30 17:44:06 +0530

    final changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/index';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -11,4 +11,4 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 // PersistGate is required to provide persisted data to the child components
 
-ReactDOM.render(<HashRouter><Provider store={store}><PersistGate loading={null} persistor={persistor}><App /></PersistGate></Provider></HashRouter>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><PersistGate loading={null} persistor={persistor}><HashRouter><App /></HashRouter></PersistGate></Provider>, document.getElementById('root'));
